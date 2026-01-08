6e4de3123d1c0232f963d693ce14e8bff150effe
zero-width-space <yoshifumi.3.14159@gmail.com>
2026-01-08 12:27:27 +0800

    Changed opening

--- opening.py
+++ opening.py
@@ -5,3 +5,13 @@ from manim import *
 class Opening(BaseSection):
     def construct(self) -> None:
         self.show_section_title("Building a chess engine", "Video made with Manim")
+        body_text = Paragraph(
+            "Made by",
+            "Li Dianheng",
+            "Tan Le Qian",
+            "Miyazaki Keishi",
+            font_size=48,
+            alignment="center",
+        )
+        self.play(Write(body_text))
+        self.fade_out()
